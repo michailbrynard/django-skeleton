772cd579cad1b8853ed6f1a2c14cbacac2ba41da
Michail Brynard <mbrynard@gmail.com>
2015-08-31 10:40:50 +0200

    Hello world.

---
+++ src/config/urls.py
@@ -0,0 +1,26 @@
+from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from filebrowser.sites import site
+
+
+admin.autodiscover()
+
+
+urlpatterns = patterns('',
+
+    # Admin Url
+    # (r'^$', RedirectView.as_view(url='/admin/')),
+
+    # Admin Url
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/filebrowser/', include(site.urls)),
+
+    # Auth API
+    url(r'^api/', include('administration.urls')),
+
+    # Api docs
+    url(r'^docs/', include('rest_framework_swagger.urls')),
+
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
